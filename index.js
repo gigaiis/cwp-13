5f4dba3d618d8b87570a31cbb660a96101f18e39
Egor <zangedzu333gig@gmail.com>
2018-10-10 20:44:22 +0300

    Create api for vehicles

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.all('/' , (req, res) =>
     res.send('cwp-13');
 });
 
-app.listen(3030, '127.0.0.1', () =>
+app.listen(3000, '127.0.0.1', () =>
 {
     console.log('Server started ...');
 });
--- routers/insert.js
+++ routers/insert.js
@@ -1,4 +1,15 @@
 module.exports = async function (db) {
+	console.log('Begin insert');
     await db.sequelize.sync({force: true});
-    return Promise.all([]);
+    return Promise.all([
+    	db.Fleet.create({
+            name: "Fleet #1"
+        }),
+        db.Fleet.create({
+            name: "Fleet #2"
+        }),
+        db.Fleet.create({
+            name: "Fleet #3"
+        })
+    ]);
 };
