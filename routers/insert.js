d33021395171eb37971d6ecb751cced150ee9ac4
Egor <zangedzu333gig@gmail.com>
2018-10-10 22:22:13 +0300

    Task 02

--- routers/insert.js
+++ routers/insert.js
@@ -23,5 +23,23 @@ module.exports = async function (db) {
             name: "Car #3",
             fleetId: 2
         }),
+        db.Motion.create({
+            latitude: 51.4103,
+            longitude: 49.49347,
+            time: "2018-10-06T15:26:56.000Z",
+            vehicleId: 1
+        }),
+        db.Motion.create({
+            latitude: 59.1132,
+            longitude: 7.1432,
+            time: "2018-10-06T15:56:56.000Z",
+            vehicleId: 1
+        }),
+        db.Motion.create({
+            latitude: 71.4103,
+            longitude: 120.49347,
+            time: "2018-10-06T16:04:00.000Z",
+            vehicleId: 1
+        })
     ]);
 };
